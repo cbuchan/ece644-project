0ba424e73bc75680e7396ed3ea385feaea218c93
Collin Buchan <cbuchan@andrew.cmu.edu>
2013-10-23 16:27:17 -0400

    moved HelloIOIO code to SquirrelBoxIOIO project

--- SquirrelBoxIOIO/src/com/squirrelbox/ioio/MainActivity.java
+++ SquirrelBoxIOIO/src/com/squirrelbox/ioio/MainActivity.java
@@ -1,22 +1,89 @@
 package com.squirrelbox.ioio;
 
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+/**
+ * This is the main activity of the HelloIOIO example application.
+ * 
+ * It displays a toggle button on the screen, which enables control of the
+ * on-board LED. This example shows a very simple usage of the IOIO, by using
+ * the {@link IOIOActivity} class. For a more advanced use case, see the
+ * HelloIOIOPower example.
+ */
+public class MainActivity extends IOIOActivity {
+	private ToggleButton button;
 
+	/**
+	 * Called when the activity is first created. Here we normally initialize
+	 * our GUI.
+	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main);
+		button = (ToggleButton) findViewById(R.id.button);
 	}
 
+	/**
+	 * This is the thread on which all the IOIO activity happens. It will be run
+	 * every time the application is resumed and aborted when it is paused. The
+	 * method setup() will be called right after a connection with the IOIO has
+	 * been established (which might happen several times!). Then, loop() will
+	 * be called repetitively until the IOIO gets disconnected.
+	 */
+	class Looper extends BaseIOIOLooper {
+		/** The on-board LED. */
+		private DigitalOutput led;
+		private DigitalOutput lock;
+
+		/**
+		 * Called every time a connection with IOIO has been established.
+		 * Typically used to open pins.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
+		 */
+		@Override
+		protected void setup() throws ConnectionLostException {
+			led = ioio_.openDigitalOutput(0, true);
+			lock = ioio_.openDigitalOutput(7, DigitalOutput.Spec.Mode.OPEN_DRAIN, false);
+		}
+
+		/**
+		 * Called repetitively while the IOIO is connected.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#loop()
+		 */
+		@Override
+		public void loop() throws ConnectionLostException {
+			led.write(!button.isChecked());
+			lock.write(!button.isChecked());
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	/**
+	 * A method to create our IOIO thread.
+	 * 
+	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
+	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected IOIOLooper createIOIOLooper() {
+		return new Looper();
 	}
-
 }
