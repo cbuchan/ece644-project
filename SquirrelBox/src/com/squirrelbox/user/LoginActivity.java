8b5ec8461f430dec7a2897d3152607a73b722a29
Collin Buchan <cbuchan@andrew.cmu.edu>
2013-10-23 17:11:07 -0400

    dummy login activity for user app

---
+++ SquirrelBox/src/com/squirrelbox/user/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.squirrelbox.user;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+	private Button loginButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		loginButton = (Button) findViewById(R.id.button_login);
+
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+}
